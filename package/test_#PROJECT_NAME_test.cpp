a551795b622a20f370c1494383af7e40368b24c2
Augusto Nalin <3024648+augusto630@users.noreply.github.com>
2025-12-28 21:24:06 -0300

    Simplified client unit test

--- package/test_#PROJECT_NAME_test.cpp
+++ package/test_#PROJECT_NAME_test.cpp
@@ -21,44 +21,20 @@ TEST(main, main_should_return_0) {
     ASSERT_EQ(result, 0);
 }
 
-TEST(main, help_should_return_1) {
-    // Arrange
-    #PROJECT_NAME::main main{};
-    std::array<const char*, 2> args{"", "-h"};
-
-    // Act
-    int result = main.run(2, args.data());
-
-    // Assert
-    ASSERT_NE(result, 0);
-}
-
-
 ///
 /// This test is not necessary but serves to illustrate how to work with test cases
 ///
 std::vector<std::tuple<std::string, int> > exitCode_TestCases{
         {"./", 0},
-        {"./ -h", 1},
-        {"./ --help", 1},
+        {"./ -h", 0},
     };
 TEST_WITH_CASES(exitCode, main_should_exit_with_expected_code, exitCode_TestCases) {
     // Arrange
     const auto [args, expected] = GetParam();
-    const auto argv = stde::string::split(args, " ");
-    const auto argc = std::size(argv);
-    #PROJECT_NAME::main main{};
-
-    std::vector<const char *> ptrArgv{};
-    ptrArgv.reserve(argv.size());
-
-    for (const auto &arg: argv) {
-        ptrArgv.push_back(arg.c_str());
-    }
 
     // Act
-    const auto result = main.run(std::size(ptrArgv), ptrArgv.data());
+    // ... Add logic here
 
     // Assert
-    ASSERT_EQ(result, expected);
+    ASSERT_EQ(0, expected);
 }
